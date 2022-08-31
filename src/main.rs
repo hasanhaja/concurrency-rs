eb4ee1738dd8de6c9c18800072208d8c484029fe
Hasan Ali <hasanhaja@gmail.com>
2022-08-31 23:17:22 +0100

    Implement sequential and multithreaded logic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,83 @@
-use std::error::Error;
+use std::{fs, io, path::PathBuf};
 
-use image::io::Reader;
+use image::{io::Reader, DynamicImage};
+use rayon::prelude::IntoParallelRefIterator;
+use rayon::iter::ParallelIterator;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let image = Reader::open("input-images/flowers_original.jpg")?.decode()?;
+type Error = Box<dyn std::error::Error>;
+type Result<T> = ::std::result::Result<T, Error>;
 
-    let image = image.grayscale();
+fn generate_inputs() -> Result<()> {
+    let image = Reader::open("assets/flowers_original.jpg")?.decode()?;
 
-    image.save("output-images/flowers_original.jpg")?;
+    for n in 0..100 {
+        image.save(format!("input-images/flowers_{}.jpg", n))?;
+    }
 
     Ok(())
+}
+
+fn paths_as_string(paths: Vec<PathBuf>) -> Result<Vec<String>> {
+    let inputs = paths
+        .iter()
+        .map(|e| e.to_str().unwrap().to_string())
+        .collect::<Vec<_>>();
+
+    Ok(inputs)
+}
+
+fn get_inputs() -> Result<Vec<PathBuf>> {
+    let inputs = fs::read_dir("input-images")?;
+    let inputs = inputs
+        .map(|res| res.map(|e| e.path()))
+        .filter(|res| res.is_ok() && res.as_ref().unwrap().is_file())
+        .collect::<::std::result::Result<Vec<_>, io::Error>>()?;
+
+    Ok(inputs)
+}
+
+fn process<F>(imagePath: &String, destination: &str, tag: &str, f: F) -> Result<()>
+where
+    F: Fn(DynamicImage) -> DynamicImage,
+{
+    let image = Reader::open(imagePath)?.decode()?;
+    let filename = imagePath.split("/").last().unwrap();
+
+    let image = f(image);
+
+    image.save(format!("{}/{}-{}", destination, tag, filename))?;
+
+    Ok(())
+}
+
+fn seq_process_images() -> Result<()> {
+    let inputs = get_inputs()?;
+    let inputs = paths_as_string(inputs)?;
+
+    inputs.iter().for_each(|path| {
+        process(path, "seq-output-images", "gray", |image| image.grayscale()).unwrap()
+    });
+
+    Ok(())
+}
+
+fn mult_process_images() -> Result<()> {
+    let inputs = get_inputs()?;
+    let inputs = paths_as_string(inputs)?;
+
+    inputs.par_iter().for_each(|path| {
+        process(path, "mult-output-images", "gray", |image| image.grayscale()).unwrap()
+    });
+
+    Ok(())
+}
+
+fn async_process_images() -> Result<()> {
+    todo!("To be implemented");
+}
+
+fn main() -> Result<()> {
+    // generate_inputs()
+    // seq_process_images()
+    mult_process_images()
 }
