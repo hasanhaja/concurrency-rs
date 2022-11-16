f33da20dc6f145c88275034b5385f49c44387e76
Hasan Ali <hasanhaja@gmail.com>
2022-11-16 02:22:31 +0000

    Async benchmark almost configured, still fighting borrow checker

--- benches/concurrency_benchmark.rs
+++ benches/concurrency_benchmark.rs
@@ -1,6 +1,8 @@
-use core::{seq_process_images, clear_outputs, mult_process_images};
+use core::{seq_process_images, clear_outputs, mult_process_images, async_process_images};
 
 use criterion::{criterion_group, criterion_main, Criterion};
+use tokio::runtime::Runtime;
+// use criterion::async_executor::FuturesExecutor;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let sigmas = vec![0.5, 3.0, 5.5];
@@ -17,6 +19,14 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             clear_outputs("mult-output-images").unwrap();
         }));
     }
+
+    // https://github.com/bheisler/criterion.rs/issues/548
+    for sigma in &sigmas {
+        c.bench_function(format!("Async ({})", sigma).as_str(), |b| b.to_async(Runtime::new().unwrap()).iter(|| async {
+            async_process_images(&sigma).await.unwrap();
+            clear_outputs("async-output-images").unwrap();
+        }));
+    } 
 }
 
 criterion_group!(benches, criterion_benchmark);
